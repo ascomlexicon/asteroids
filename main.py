c93f6cd2fa8d9abc9b4b4196d026cb54d28da392
Jethro Ramos <jethro.ramos@proton.me>
2024-09-07 00:23:09 +0100

    Cap game to run at 60 FPS

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from constants import *
 
 def main():
     pygame.init()
+    
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -12,6 +15,8 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
