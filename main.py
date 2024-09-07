1f6950102ab271a1b8ccd24c09e1f28e7f85c20c
Jethro Ramos <jethro.ramos@proton.me>
2024-09-07 01:58:05 +0100

    Add asteroid spawning logic

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     
+    asteroids = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
     
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) 
+    asteroid_field = AsteroidField()
 
     dt = 0
 
