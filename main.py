c878464e92d24a80ab722b9a20002ae6d850c6df
Jethro Ramos <jethro.ramos@proton.me>
2024-09-07 01:22:52 +0100

    Refactor game loop to use groups

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from player import Player
 def main():
     pygame.init()
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) 
@@ -17,10 +22,13 @@ def main():
                 return
         
         screen.fill("black")
-        
-        player.update(dt)
-        player.draw(screen)
 
+        for sprite in updatable:
+            sprite.update(dt)
+
+        for sprite in drawable:
+            sprite.draw(screen)
+        
         pygame.display.flip()
 
         # Framerate cap of 60 FPS
